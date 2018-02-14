76f6ca8de6a8ef40c2f8eefeeeff97b81f3d9ac9
Aaron Perley <aperley@andrew.cmu.edu>
2018-02-13 23:54:33 -0500

    Remove and ignore build files

--- Source/framework/build/bootloader/soc/esp32/soc_memory_layout.d
+++
@@ -1,3 +0,0 @@
-esp32/soc_memory_layout.o: \
- /home/ro269/esp/esp-idf/components/soc/esp32/soc_memory_layout.c
-/home/ro269/esp/esp-idf/components/soc/esp32/soc_memory_layout.c:
--- Source/framework/build/expat/library/xmltok_impl.d
+++
@@ -1,3 +0,0 @@
-library/xmltok_impl.o: \
- /home/ro269/esp/esp-idf/components/expat/library/xmltok_impl.c
-/home/ro269/esp/esp-idf/components/expat/library/xmltok_impl.c:
